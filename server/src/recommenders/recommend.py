bdc1e9554731ed123e99f60fad23ac98d22ceec8
Jatin Gupta <jatingupta6171@gmail.com>
2023-08-17 23:27:47 +0530

    Update recommend.py

--- server/src/recommenders/recommend.py
+++ server/src/recommenders/recommend.py
@@ -1,5 +1,6 @@
-print("START")
-
+from Book_Recommendation.utils import recommend_items_knn
+import pickle
+import pandas as pd
 import tensorflow
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.layers import GlobalMaxPooling2D
@@ -11,7 +12,40 @@ from tqdm import tqdm
 import pickle
 # import cv2
 from sklearn.neighbors import NearestNeighbors
+from PIL import Image
+import urllib.request
 
+# -------------------------------------------------------------------
+# Books
+
+# load model
+books_model_knn_loaded = pickle.load(open('/content/drive/MyDrive/Book-Recommendation/models/KNN_Books_Item-Item.h5', 'rb'))
+
+# load pivot_matrix
+df_pivot = pd.read_csv('/content/drive/MyDrive/Book-Recommendation/matrix/df_pivot.csv')
+df_pivot.set_index('isbn', inplace=True)
+
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+
+# Clothing
+model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
+
+embeddings = np.array(pickle.load(open('embeddings.pkl', 'rb')))
+images = pickle.load(open('filenames.pkl', 'rb'))
+
+knn = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
+knn.fit(embeddings)
+
+print('KNN DONE')
+
+# -------------------------------------------------------------------
 
 def get_features(img_path, model):
     img = image.load_img(img_path,target_size=(224,224))
@@ -44,28 +78,17 @@ def recommend_items(features, knn, images):
 
 
 
-model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
-model.trainable = False
 
-model = tensorflow.keras.Sequential([
-    model,
-    GlobalMaxPooling2D()
-])
+def similar_books(isbn_id):
+    result = recommend_items_knn(books_model_knn_loaded, df_pivot, isbn_id)
+    print(result)
+    # return result
 
-print("ENTRY")
-
-
-embeddings = np.array(pickle.load(open('embeddings.pkl', 'rb')))
-images = pickle.load(open('filenames.pkl', 'rb'))
-
-knn = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
-knn.fit(embeddings)
-
-print('KNN DONE')
-
-# path to input_image
-img_path = 'images/1245.jpg'
-features = get_features(img_path, model)
-# print(features, '\n\n\n')
-recommended_images = recommend_items(features, knn, images)
-print(recommended_images)
+def similar_image_products(img_url):
+    img_path = 'img.png'
+    urllib.request.urlretrieve(img_url, 'img.jpg')
+    features = get_features(img_path, model)
+    # print(features, '\n\n\n')
+    recommended_images = recommend_items(features, knn, images)
+    print(recommended_images)
+    return recommended_images
